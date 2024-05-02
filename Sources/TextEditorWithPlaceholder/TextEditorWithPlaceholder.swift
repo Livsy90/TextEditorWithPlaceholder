7d0fc077f3e89c6f5d92917c756d6a18c32b361e
Artem <60046989+Livsy90@users.noreply.github.com>
2024-05-02 11:35:45 +0400

    TextEditorWithPlaceholder

--- Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
+++ Sources/TextEditorWithPlaceholder/TextEditorWithPlaceholder.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct TextEditorWithPlaceholder: View {
+    
+    @State private var text = ""
+    private let placeholder: String
+    
+    public init(
+        text: String = "",
+        placeholder: String
+    ) {
+        
+        self.text = text
+        self.placeholder = placeholder
+    }
+    
+    public var body: some View {
+        TextEditor(text: $text)
+            .overlay(alignment: .topLeading) {
+                Text(placeholder)
+                    .foregroundColor(Color(uiColor: .placeholderText))
+                    .multilineTextAlignment(.leading)
+                    .opacity(text.isEmpty ? 1 : 0)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 4)
+                    .allowsHitTesting(false)
+            }
+    }
+}
